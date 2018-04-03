0fc559fc6832d4dfb612a72c37b336ac23a961a9
TharinduDG <tdgalappaththi@gmail.com>
2018-04-03 10:45:28 +0530

    Update count status to stop when count reaches zero

--- public/bundle.js
+++ public/bundle.js
@@ -25638,6 +25638,19 @@
 	      }
 	    }
 	  },
+	  // componentDidMount: function () {
+	  //   console.log('componentDidMount');
+	  // },
+	  // componentWillMount: function () {
+	  //   console.log('componentWillMount');
+	  // },
+	  // componentWillUpdate: function (nextProps, nextState){
+	  //
+	  // },
+	  componentWillUnmount: function componentWillUnmount() {
+	    clearInterval(this.timer);
+	    this.timer = undefined;
+	  },
 	  startTimer: function startTimer() {
 	    var _this = this;
 
@@ -25646,6 +25659,10 @@
 	      _this.setState({
 	        count: newCount >= 0 ? newCount : 0
 	      });
+
+	      if (newCount == 0) {
+	        _this.setState({ countdownStatus: 'stopped' });
+	      }
 	    }, 1000);
 	  },
 	  handleSetCountdown: function handleSetCountdown(seconds) {
@@ -25798,6 +25815,9 @@
 	      _this.props.onStatusChange(newStatus);
 	    };
 	  },
+	  // componentWillReceiveProps(newProps){
+	  //   console.log('componentWillReceiveProps', newProps.countdownStatus);
+	  // },
 	  render: function render() {
 	    var _this2 = this;
 
